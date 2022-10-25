a68d8ca7d98de5b9c4859356a5e292a64ae65130
pedro011hrq <107210071+pedro011hrq@users.noreply.github.com>
2022-10-25 20:36:32 -0300

    Atualizando o commit

--- script.js
+++ script.js
@@ -42,3 +42,35 @@ console.log(array.indexOf(true));
 //slice - retorna uma parte de um array existente
 let novoArray = array.slice(0, 2);
 console.log(novoArray);
+
+console.log('----------------------------')
+// o que são objetos?
+// como declara um objeto
+let object = {propriedade1: 'valo1', propriedade2: 'valor2'};
+// deve ser declarado entre chaves {}
+console.log(object);
+
+//assim como arrays as propriedades de objetos tambem podem guardar quaisquer valores*/
+let object2 = {
+    string: 'string',
+    number: 1,
+    boolean: true,
+    array: ['array'],
+    objectInterno: {objetoInterno: 'objetoInterno'}};
+
+// como acessar propriedades de objetos
+// usando o ponto "."
+console.log(object2.objectInterno);
+
+// desestruturando objeto
+let string = object2.string;
+let number = object2.number;
+let boolean = object2.boolean;
+
+console.log(string, number, boolean);
+
+
+
+
+
+
